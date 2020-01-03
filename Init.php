3dcd398609cc310e65c8f5c5880644571de2b762
Timm Mark Jensen <tje@simplesolution.dk>
2020-01-03 15:21:18 +0100

    updated init

--- Init.php
+++ Init.php
@@ -1,11 +1,6 @@
 <?php
 
-namespace ssoFramework;
-
-require_once __DIR__ . '/src/cmb2/init.php';
-require_once __DIR__ . '/src/cmb2/cmb2-post-search-field/cmb2_post_search_field.php';
-
-
+namespace SimpleSolution\WpInteractionFramework;
 
 class Init {
 
@@ -13,31 +8,5 @@ class Init {
 	}
 
 	static function theme_setup() {
-		add_action( 'after_setup_theme', function () {
-			add_theme_support( 'title-tag' );
-
-			add_theme_support( 'custom-logo', [
-				'flex-width'  => true,
-				'flex-height' => true,
-				'header-text' => false
-			] );
-
-			add_theme_support( 'html5', [
-				'search-from' => true
-			] );
-
-			add_theme_support( 'post-formats', array( 'video' ) );
-
-
-			add_theme_support( 'post-thumbnails' );
-
-
-
-			// Register nav menu locations
-			register_nav_menu( 'main-menu', 'Main Menu' );
-			register_nav_menu( 'top-menu', 'Top Menu' );
-
-			load_theme_textdomain( 'sso-showbizz', get_template_directory() . '/languages' );
-		} );
 	}
 }
